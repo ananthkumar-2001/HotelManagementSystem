82bc1c85d120e800e87a49d816e17ae9a7b47bb9
Ananth Kumar <pvananthkumar@gmail.com>
2022-05-17 01:02:10 +0530

    ZuulGateway

---
+++ ZullGateway/src/main/java/filters/ErrorFilter.java
@@ -0,0 +1,43 @@
+package filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Configuration
+public class ErrorFilter extends ZuulFilter{
+	
+	Logger logger = LoggerFactory.getLogger(ErrorFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		RequestContext ctx = RequestContext.getCurrentContext();
+        String response = ctx.getResponseBody();
+        logger.info("Error occurred, Response  = {}, ", response);
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "error";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
---
+++ ZullGateway/src/main/java/filters/PostFilter.java
@@ -0,0 +1,61 @@
+package filters;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.CharEncoding;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.io.CharStreams;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Configuration
+public class PostFilter extends ZuulFilter{
+	
+	Logger logger = LoggerFactory.getLogger(PostFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		RequestContext ctx = RequestContext.getCurrentContext();
+
+        HttpServletResponse response = ctx.getResponse();
+        //ctx.getResponse().setStatus(500);
+        logger.info("Response  Status= {}", response.getStatus());
+
+        try (InputStream is = ctx.getResponseDataStream()) {
+            String respData = CharStreams.toString(new InputStreamReader(is, CharEncoding.UTF_8));
+            logger.info("Response  Data = {}", respData);
+            ctx.setResponseBody(respData);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "post";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
